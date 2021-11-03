976a8539c3a2ce6ced7a909ef90596649d5b17df
raheelbaig <68862554+raheelbaig@users.noreply.github.com>
2021-11-03 23:15:36 +0500

    push

--- app.mjs
+++ app.mjs
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from 'cors'
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 let users = [];
+app.use(cors());
 app.use(express.json())
 app.get("/users",(req,res)=>{
     res.send(users)
