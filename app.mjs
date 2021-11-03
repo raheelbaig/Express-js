3b244790265bb014b37de5ed8f04bbd84a6c3a90
raheelbaig <68862554+raheelbaig@users.noreply.github.com>
2021-11-03 22:24:29 +0500

    push all

---
+++ app.mjs
@@ -0,0 +1,75 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.port || 3000;
+app.listen(3000, () => {
+    console.log(`server is running on port ${PORT}`);
+});
+let users = [];
+app.use(express.json())
+app.get("/users",(req,res)=>{
+    res.send(users)
+});
+
+
+app.post("/users",(req,res)=>{
+    if(!req.body.name ||  !req.body.email || !req.body.pass)
+    {
+        res.status(400).send("invalid data");
+    }
+    else{
+       users.push({
+        name :req.body.name,
+        email :req.body.email,
+        pass :req.body.pass
+       })
+    }
+    res.send("user created")
+});
+app.put("/users/:id",(req,res)=>{
+
+    if (users[req.params.id]) {
+
+        if (req.body.name) {
+          users[req.params.id].name = req.body.name
+        }
+        if (req.body.email) {
+          users[req.params.id].email = req.body.email
+        }
+        if (req.body.pass) {
+          users[req.params.id].pass = req.body.pass
+        }
+    
+        res.send(users)
+    
+      } else {
+        res.send("user not found");
+      }
+// jdvjnjnvnjnjn
+
+
+
+});
+app.delete("/users/:id",(req,res)=>{
+    if(users[req.params.id]){
+      users[req.params.id] = {};
+    res.send("user deleted");
+
+  } else {
+    res.send("user not found");
+  }
+    });
+
+
+
+
+
+
+
+
+
+
+app.get("/about",(req,res)=>{
+    res.send("Kamran here how can i do for you!!")
+});
+
